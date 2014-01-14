d0b50be2ff973db83c1a838103df914d63c3d8e8
Totte Karlsson <tottek@gmail.com>
2014-01-13 22:20:31 -0800

    Renamed manager files

---
+++ manager/telPluginUtils.cpp
@@ -0,0 +1,20 @@
+#pragma hdrstop
+#include "telPluginUtils.h"
+#include "rr/rrStringList.h"
+//---------------------------------------------------------------------------
+namespace rrp
+{
+using namespace std;
+
+
+rr::StringList getRecordsAsStrings(const vector<rr::SelectionRecord>& _sels)
+{
+    rr::StringList sels;
+    for(int i = 0; i < _sels.size(); i++)
+    {
+        sels.add(_sels[i].to_string());
+    }
+    return sels;
+}
+
+}
