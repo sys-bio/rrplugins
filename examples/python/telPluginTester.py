543042d758b2f7458bdc4c96cbb744bf8dbcb12f
Totte Karlsson <tottek@gmail.com>
2014-01-17 12:05:28 -0800

    Updated plugin tester to print information about each plugin

--- examples/python/telPluginTester.py
+++ examples/python/telPluginTester.py
@@ -7,7 +7,6 @@ plugins in the following folder: ' + gDefaultPluginsPath
 
 pm = createPluginManager()
 
-
 # Load plugins from the plugin folder
 result = loadPlugins(pm)
 if not result:
@@ -18,15 +17,20 @@ if not result:
 print 'Number of Plugins: ' + `getNumberOfPlugins(pm)`
 print 'Plugin Names: ' + `getPluginNames(pm)`
 
-# Get a handle to a plugin
+#Go trough all plugins and print information
 aPlugin = getFirstPlugin(pm)
+while True:
+    if aPlugin == None:
+        break
+        
+    print '=========================================='
+    print 'PluginName: ' + `getPluginName(aPlugin)`
+    print 'Plugin Description: \n'
+    print getPluginDescription(aPlugin)
+    print '\nPluginParameters: ' + `getListOfPluginPropertyNames(aPlugin)`                    
+    
+    #get next plugin
+    aPlugin = getNextPlugin(pm)
 
-if(aPlugin == None):
-    print 'Failed to get a handle to a plugin'
-    exit()
-
-print 'PluginName: ' + `getPluginName(aPlugin)`
-print getPluginInfo(aPlugin)
-print 'PluginParameters: ' + `getListOfPluginPropertyNames(aPlugin)`
 print `unLoadPlugins(pm)`
 print "done"
