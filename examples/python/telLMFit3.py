c3e1009c8c71c1f7c12af8608d71fa522ad9aebc
TotteKarlsson <tottek@gmail.com>
2014-02-05 14:16:09 -0800

    Added parameter scan plugin

--- examples/python/telLMFit3.py
+++ examples/python/telLMFit3.py
@@ -6,11 +6,11 @@ import telplugins as tel
 import ctypes as ct
 
 try:
-        
-    sbmlmPluginodel = "sbml_test_0001.xml" 
+
+    sbmlmPluginodel = "sbml_test_0001.xml"
     # Create a roadrunner instance and create some data
     rr = roadrunner.RoadRunner()
-    rr.load(sbmlmPluginodel)    
+    rr.load(sbmlmPluginodel)
     data = rr.simulate(0, 10, 15000) # Want 512 points
 
     #Add noise to the data
@@ -28,54 +28,53 @@ try:
     # Add the noise
     noisePlugin.execute()
     fName = 'testData3.dat'
-    noisePlugin.InputData.writeDataSeries (fName)       
+    noisePlugin.InputData.writeDataSeries (fName)
     #===================================================================
-    
+
     lmPlugin = tel.Plugin ("tel_lm")
-    experimentalData = tel.DataSeries.readDataSeries (fName)    
-       
+    experimentalData = tel.DataSeries.readDataSeries (fName)
+
     lmPlugin.ExperimentalData = experimentalData;
     lmPlugin.SBML = lmPlugin.readAllText(sbmlmPluginodel)
-    
+
     # Add the parameters that we're going to fit and the initial value
-    lmPlugin.InputParameterList = ["k1", .1]   
+    lmPlugin.InputParameterList = ["k1", .1]
     lmPlugin.FittedDataSelectionList = "[S1] [S2]"
     lmPlugin.ExperimentalDataSelectionList = "[S1] [S2]"
-        
+
     # Execute lmPluginfit plugin
     res = lmPlugin.execute()
-        
-    # Get the residuals    
-    residuals  = lmPlugin.Residuals.toNumpy               
+
+    # Get the residuals
+    residuals  = lmPlugin.Residuals.toNumpy
     residuals = residuals[:,[1,2]]
-    
+
     #Plot as a histogram
     plt.hist(residuals, 50, normed=True)
     plt.show()
-        
+
     stdResiduals = lmPlugin.StandardizedResiduals.toNumpy
-    stdResiduals = stdResiduals[:,[1,2]]            
+    stdResiduals = stdResiduals[:,[1,2]]
     plt.hist(stdResiduals, 50, normed=True)
     plt.show()
-          
+
     #Plot normal probability plots
     probPlots = lmPlugin.NormalProbabilityOfResiduals.toNumpy
-    print probPlots
-    
+
     x1 = probPlots[:,[0]]
-    y1 = probPlots[:,[1]]    
-        
+    y1 = probPlots[:,[1]]
+
     x2 = probPlots[:,[2]]
-    y2 = probPlots[:,[3]]    
-    
+    y2 = probPlots[:,[3]]
+
     lineX = np.arange(-5, 5, .1)
     lineY = lineX
-    plt.plot(lineX, lineY, '-r')         
+    plt.plot(lineX, lineY, '-r')
     plt.plot(x1,y1, 'o')
-    plt.plot(x2,y2, 'x')    
-    #plt.scatter(x,y)    
+    plt.plot(x2,y2, 'x')
+    #plt.scatter(x,y)
     plt.show()
-        
+
 except Exception as e:
     print 'Exception: ' + `e`
 
---
+++ plugins/parameter_scan/csChiSquare.h
@@ -0,0 +1,47 @@
+#ifndef csChiSquareH
+#define csChiSquareH
+#include <vector>
+#include "telProperty.h"
+#include "telCPPPlugin.h"
+#include "csChiWorker.h"
+//---------------------------------------------------------------------------
+
+using namespace tlp;
+using std::string;
+
+class ChiSquare : public CPPPlugin
+{
+    friend class ChiWorker;
+
+    public:
+        //Input
+        Property<int>                           mNrOfModelParameters;
+        Property<TelluriumData>				    mExperimentalData;
+        Property<TelluriumData>			        mModelData;
+
+        //Output
+        Property<double>			            mChiSquare;                     //Chi square
+        Property<double>			            mReducedChiSquare;              //Reduced Chi Square
+
+    protected:
+        ChiWorker                               mWorker;
+
+    public:
+                                                ChiSquare();
+                                               ~ChiSquare();
+        bool                                    execute(bool inThread = false);
+        bool                                    resetPlugin();
+        string                                  getImplementationLanguage();
+        bool                                    isWorking() const;
+        unsigned char*                          getManualAsPDF() const;
+        unsigned int                            getPDFManualByteSize();
+        void                                    assignPropertyDescriptions();
+};
+
+extern "C"
+{
+RR_PLUGIN_DECLSPEC ChiSquare*   plugins_cc       createPlugin();
+RR_PLUGIN_DECLSPEC const char*  plugins_cc       getImplementationLanguage();
+}
+
+#endif
