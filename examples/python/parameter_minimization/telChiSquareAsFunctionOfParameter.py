a1e49d56d3a46548610c38057add561daf5fcb67
Totte Karlsson <tottek@gmail.com>
2014-02-10 17:55:37 -0800

    - Updated python script to look at chi square as a function of a parameter

--- examples/python/parameter_minimization/telChiSquareAsFunctionOfParameter.py
+++ examples/python/parameter_minimization/telChiSquareAsFunctionOfParameter.py
@@ -1,4 +1,6 @@
 import roadrunner
+import numpy as np
+import matplotlib.pyplot as plt
 import telplugins as tel
 
 try:
@@ -7,8 +9,7 @@ try:
     
     #This does not look right.. but it works..
     expData = expData.readDataSeries('ExperimentalData.dat')   
-    expData.plot()
-    
+        
     modelPlugin = tel.Plugin("tel_sbml_model")   
     sbml_model  = modelPlugin.Model
     
@@ -16,26 +17,36 @@ try:
     rr = roadrunner.RoadRunner()
     rr.load(sbml_model)
     
-    #Simulate using the same numbers as in the 'Experimental data    
-    data = rr.simulate(0, 4.5, 200) # Want 512 points
-
-    roadrunner.plot(data)
+    #Simulate using the same numbers as in the 'Experimental data
+    x = np.array([])
+    y = np.array([])     
+    for k in np.arange(0.8, 1.3, .01):      
+          
+        rr.setValue("k1", k)   
+        rr.reset() 
+        data = rr.simulate(0, 4.5, 200) # Want 512 points
+        #roadrunner.plot(data)
     
-    #Get chi square
-    chiSquare =  tel.Plugin("tel_chisquare")
+        #Get chi square
+        chiSquare =  tel.Plugin("tel_chisquare")
     
-    chiSquare.ExperimentalData = expData
-    chiSquare.ModelData = tel.getDataSeries(data)
-    chiSquare.NrOfModelParameters = 1
+        chiSquare.ExperimentalData = expData
+        chiSquare.ModelData = tel.getDataSeries(data)
+        chiSquare.NrOfModelParameters = 1
     
-    if not chiSquare.execute():
-        raise Exception( tel.getLastError() )
+        if not chiSquare.execute():
+            raise Exception( tel.getLastError() )
+               
+        redChi = chiSquare.ReducedChiSquare.getElement(0,1)               
+        print  'k, chisquare = ' + `k` +', '+  `redChi` 
+        x = np.append(x, k) 
+        y = np.append(y, redChi)         
     
-    print chiSquare.ChiSquare.cols
-    print chiSquare.ChiSquare.rows
-    print chiSquare.ChiSquare.getElement(0,1)
-    print chiSquare.ReducedChiSquare.getElement(0,1)        
+    plt.plot(x,y, '-o', label="Reduced ChiSquare")
+    plt.legend()
+    plt.xlabel("k1")
     
+    plt.show()
     print "done"
 
 except Exception as e:
