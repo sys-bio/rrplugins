da3f9772ec542ace65bede674ee294bbfad63ef2
Totte Karlsson <tottek@gmail.com>
2014-03-02 14:33:26 -0800

    - Added some more files

---
+++ examples/python/basic/telCreateNoisyDataDoxyPage.py
@@ -0,0 +1,34 @@
+import roadrunner
+import telplugins as tel
+
+noisePlugin = tel.Plugin ("tel_add_noise")
+
+print noisePlugin.name()
+print noisePlugin.hint()
+print noisePlugin.description()
+
+print noisePlugin.listOfProperties()
+modelPlugin= tel.Plugin("tel_sbml_model")        
+sbml_model = modelPlugin.Model
+
+# Create a roadrunner instance
+rr = roadrunner.RoadRunner()
+rr.load(sbml_model)
+
+# Generate data
+data = rr.simulate(0, 10, 511) # Want 512 points
+
+# The plugin will need a handle to the underlying roadrunner data
+d = tel.getDataSeries (data)
+
+noisePlugin.InputData = d
+
+# Get parameter for the 'size' of the noise
+noisePlugin.Sigma = 1.e-5
+
+noisePlugin.execute ()
+
+noisePlugin.InputData.plot ()
+tel.show()
+
+print "done"
