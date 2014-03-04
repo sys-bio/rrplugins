dbc9d1741ea32219d59060cec5b1fc62072fb0e3
Totte Karlsson <tottek@gmail.com>
2014-03-04 12:46:54 -0800

    - Initial commit of Nelder Mead plugin sources

---
+++ plugins/beta/nelder_mead/nmUtils.cpp
@@ -0,0 +1,16 @@
+#pragma hdrstop
+#include <sstream>
+#include <string>
+#include "telUtils.h"
+#include "telLogger.h"
+#include "nmUtils.h"
+#include "lm.h"
+#include "lib/lmmin.h"
+//---------------------------------------------------------------------------
+
+namespace lmfit
+{
+using namespace std;
+
+}
+
