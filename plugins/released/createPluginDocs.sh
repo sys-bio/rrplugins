a7107660ce604ade4aa6a4e7989df9cf7dcae493
Totte Karlsson <tottek@gmail.com>
2014-03-29 10:38:46 -0700

    Updated latex docs

--- plugins/released/createPluginDocs.sh
+++ plugins/released/createPluginDocs.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 
-plugins="add_noise test_model chisquare levenberg_marquardt nelder_mead monte_carlo_bs"
+plugins="add_noise test_model chisquare levenberg_marquardt nelder_mead monte_carlo_bs auto2000"
 #Create PDF and HTML docs for each plugin
 
 thisFolder=`pwd`
