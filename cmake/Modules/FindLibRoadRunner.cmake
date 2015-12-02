08dc4712a167495e05ccc5ff0ad540f1f772b4ca
0u812 <0u812@users.noreply.github.com>
2015-12-02 11:03:50 -0800

    add find module for roadrunner

---
+++ cmake/Modules/FindLibRoadRunner.cmake
@@ -0,0 +1,59 @@
+#.rst:
+# FindLibRoadRunner
+# -----------
+#
+# Try to find libRoadRunner, an SBML-compliant simulation library.
+# Set LIBROADRUNNER_PATH to give a hint about where to search.
+#
+# Once done this will define
+#
+# ::
+#
+#   LIBROADRUNNER_FOUND - System has LibXml2
+#   LIBROADRUNNER_INCLUDE_DIR - The libRoadRunner include directory
+#   LIBROADRUNNER_LIBRARIES - The libraries needed to use libRoadRunner
+#   LIBROADRUNNER_DEFINITIONS - Compiler switches required for using libRoadRunner
+#   LIBROADRUNNER_VERSION_STRING - the version of libRoadRunner found
+
+#=============================================================================
+# Copyright 2015 J. Kyle Medley
+#
+# Licensed under the Apache License, Version 2.0
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#
+# Based on FindLibXml2.cmake
+#
+#=============================================================================
+
+# find version info file
+find_file(LIBROADRUNNER_VERSION_FILE NAME "VERSION.txt"
+  HINTS
+  ${LIBROADRUNNER_PATH}
+  )
+
+if(LIBROADRUNNER_VERSION_FILE AND EXISTS "${LIBROADRUNNER_VERSION_FILE}" AND NOT IS_DIRECTORY ${LIBROADRUNNER_VERSION_FILE})
+  file(STRINGS "${LIBROADRUNNER_VERSION_FILE}" LIBROADRUNNER_VERSION_STRING
+      REGEX "^[0-9]+.[0-9]+.[0-9]+")
+endif()
+
+find_path(LIBROADRUNNER_INCLUDE_DIR NAMES rrRoadRunner.h
+  HINTS
+  ${LIBROADRUNNER_PATH}/include/rr
+  )
+
+find_library(LIBROADRUNNER_LIBRARIES NAMES roadrunner
+  HINTS
+  ${LIBROADRUNNER_PATH}/lib
+  )
+
+# handle the QUIETLY and REQUIRED arguments and set LIBROADRUNNER_FOUND to TRUE if
+# all listed variables are TRUE
+include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibRoadRunner
+                                  REQUIRED_VARS LIBROADRUNNER_LIBRARIES LIBROADRUNNER_INCLUDE_DIR
+                                  VERSION_VAR LIBROADRUNNER_VERSION_STRING)
+
+mark_as_advanced(LIBROADRUNNER_INCLUDE_DIR LIBROADRUNNER_LIBRARIES)
