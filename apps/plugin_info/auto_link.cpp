265bbb6d7dd841fbb2ada67117b34201807fe7a4
0u812 <0u812@users.noreply.github.com>
2015-12-02 21:24:41 -0800

    deleted auto_link files

--- apps/plugin_info/auto_link.cpp
+++
@@ -1,3 +0,0 @@
-#pragma comment(lib, "roadrunner.lib")
-#pragma comment(lib, "telplugins_core.lib")
-#pragma comment(lib, "telplugins_common.lib")
--- testing/c/handle_manager/auto_link.cpp
+++
@@ -1,3 +0,0 @@
-#pragma comment(lib, "roadrunner.lib")
-#pragma comment(lib, "telplugins_common.lib")
-#pragma comment(lib, "telplugins_c_api.lib")
