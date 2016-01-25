2eb9e2cddbb70e564620710a568c8f98811f9333
0u812 <0u812@users.noreply.github.com>
2016-01-25 14:11:12 -0800

    throw when string prop handle is bad

--- wrappers/python/telplugins_c_api.py
+++ wrappers/python/telplugins_c_api.py
@@ -1101,7 +1101,8 @@ def getStringProperty (propertyHandle):
 rrpLib.tpSetStringProperty.restype = c_bool
 rrpLib.tpSetStringProperty.argtypes = [c_void_p, c_char_p]
 def setStringProperty(propertyHandle, value):
-    return rrpLib.tpSetStringProperty(propertyHandle, c_char_p(value))
+    if not rrpLib.tpSetStringProperty(propertyHandle, c_char_p(value)):
+        raise TypeError('Failed to set string property')
 
 ## \brief Get the list value for a property
 ## \param propertyHandle to a property instance
